ba4452b3e9e0b380a91339aa87cbd1b84bdcca25
james-zhang-hub <jameszhang2@sbcglobal.net>
2020-08-24 09:48:36 -0500

    Enable mamacita's and my phone number

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -8,8 +11,10 @@ from src.sms_service import send_kitties_update_sms
 
 KITTY_IDS = {'A821879': 'Jackson',
              'A821878': 'Betty'}
-RECIPIENT_NUMBERS = ['+12145572892']
-# RECIPIENT_NUMBERS = ['+16236806997', '+12145572892']
+RECIPIENT_NUMBERS = ['+16236806997', '+12145572892']
+
+# KITTY_IDS = json.loads(sys.argv[1])
+# RECIPIENT_NUMBERS = sys.argv[2].split(',')
 
 animal_outcomes = get_animal_outcome_data()
 
