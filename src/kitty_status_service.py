30038fe5bb62756fe239ce5589ad4cd671ffcc2f
james-zhang-hub <jameszhang2@sbcglobal.net>
2020-09-03 13:00:01 -0500

    Check if outcome_subtype exists

--- src/kitty_status_service.py
+++ src/kitty_status_service.py
@@ -2,13 +2,15 @@ from dateutil.parser import parse
 
 
 class KittyStatus:
-    def __init__(self, kitty_name, kitty_status_data=None, is_adopted=False):
+    def __init__(self, kitty_name, kitty_status_data=None, is_adopted=False, outcome_subtype='No Subtype'):
         if is_adopted:
+            self.outcome_subtype = outcome_subtype
             self.name = kitty_name
             self.id = kitty_status_data['animal_id']
             self.outcome_type = kitty_status_data['outcome_type']
             self.date = parse(kitty_status_data['datetime'])
-            self.outcome_subtype = kitty_status_data['outcome_subtype']
+            if 'outcome_subtype' in kitty_status_data.keys():
+                self.outcome_subtype = kitty_status_data['outcome_subtype']
             self.is_adopted = is_adopted
         else:
             self.name = kitty_name
